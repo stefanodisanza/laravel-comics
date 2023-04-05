0331f11d08f2390939a4203ac0689ab415ae80d1
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-04-05 16:04:42 +0200

    create something

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div>
+                <img src="/dc-logo.png" alt="" class="logo">
+            </div>
+            <div>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>Tv</li>
+                    <li>Games</li>
+                    <li>Collectibles</li>
+                    <li>Videos</li>
+                    <li>Fans</li>
+                    <li>News</li>
+                    <li>Shop</li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
